5c4d048d50c524b4be4c5b7838d5096e43a4b100
Tony5897 <tony.martinez5897@gmail.com>
2022-03-22 21:35:16 -0700

    Printing hello-world in python.

--- script.py
+++ script.py
@@ -1,2 +1,8 @@
+intro_one = "Hello World"
+mean_it = "!"
+new_lang = " Time to Learn"
+print("This is when you say " + intro_one + mean_it + new_lang + mean_it)
+# Is is it hashtag or a pound sign? Neither. It's how you comment!
 my_name = "Tony Martinez"
-print("Hello and welcome " + my_name + "!")
+print("And hello and welcome " + my_name + "!")
+# This is a demostration of how comment within a python program.
